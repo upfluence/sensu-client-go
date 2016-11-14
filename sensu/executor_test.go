8cc9d732734c7ece6baef945fc1f43cb137639f0
Mihai Todor <todormihai@gmail.com>
2016-11-14 01:05:01 +0000

    Add executor tests

---
+++ sensu/executor_test.go
@@ -0,0 +1,65 @@
+package sensu
+
+import (
+	"testing"
+
+	stdCheck "github.com/upfluence/sensu-client-go/Godeps/_workspace/src/github.com/upfluence/sensu-go/sensu/check"
+	"github.com/upfluence/sensu-client-go/sensu/check"
+	"github.com/upfluence/sensu-client-go/sensu/handler"
+)
+
+func validateCheckOutput(checkRequest *stdCheck.CheckRequest, expectedOutput *stdCheck.CheckOutput, t *testing.T) {
+	output, err := executeCheck(checkRequest)
+
+	if err != nil {
+		t.Fatalf("Expected error to be nil but got \"%s\" instead!", err)
+	}
+
+	if output == nil {
+		t.Fatalf("Expected non-nil output but got nil instead!")
+	}
+
+	if output.Status != expectedOutput.Status {
+		t.Errorf("Expected status to be \"%d\" but got \"%d\" instead!", expectedOutput.Status, output.Status)
+	}
+
+	if output.Output != expectedOutput.Output {
+		t.Errorf("Expected output to be \"%d\" but got \"%d\" instead!", expectedOutput.Output, output.Output)
+	}
+}
+
+func TestExecuteExtensionCheck(t *testing.T) {
+	check.Store["extension_check"] = &check.ExtensionCheck{Function: func() check.ExtensionCheckResult {
+		return handler.Ok("Test")
+	}}
+
+	validateCheckOutput(&stdCheck.CheckRequest{Check: &stdCheck.Check{Extension: "extension_check"}},
+		&stdCheck.CheckOutput{Status: 0, Output: "OK: Test"}, t)
+}
+
+func TestExecuteNamedExtensionCheck(t *testing.T) {
+	check.Store["named_extension_check"] = &check.ExtensionCheck{Function: func() check.ExtensionCheckResult {
+		return handler.Ok("Test")
+	}}
+
+	validateCheckOutput(&stdCheck.CheckRequest{Check: &stdCheck.Check{Name: "named_extension_check"}},
+		&stdCheck.CheckOutput{Status: 0, Output: "OK: Test"}, t)
+}
+
+func TestExecuteExternalCheck(t *testing.T) {
+	validateCheckOutput(&stdCheck.CheckRequest{Check: &stdCheck.Check{Command: "printf Test"}},
+		&stdCheck.CheckOutput{Status: 0, Output: "Test"}, t)
+}
+
+func TestExecuteEmptyCheck(t *testing.T) {
+
+	output, err := executeCheck(&stdCheck.CheckRequest{Check: &stdCheck.Check{}, Issued: 1479057736})
+
+	if err != commandKeyError {
+		t.Fatalf("Expected error to be \"%s\" but got \"%s\" instead!", commandKeyError, err)
+	}
+
+	if output != nil {
+		t.Fatalf("Expected output to be nil but got \"%+v\" instead!", output)
+	}
+}
