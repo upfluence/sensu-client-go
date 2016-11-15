1d41b80195d0683a6b09989f3dbe63d5edc3d56c
Mihai Todor <todormihai@gmail.com>
2016-11-15 15:22:46 +0000

    Add config unit tests

---
+++ sensu/config_test.go
@@ -0,0 +1,106 @@
+package sensu
+
+import (
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/upfluence/sensu-client-go/Godeps/_workspace/src/github.com/upfluence/sensu-go/sensu/check"
+	stdClient "github.com/upfluence/sensu-client-go/Godeps/_workspace/src/github.com/upfluence/sensu-go/sensu/client"
+)
+
+func validateStringParameter(actualRabbitMqUri string,
+	expectedRabbitMqUri string,
+	parameterName string,
+	t *testing.T) {
+	if actualRabbitMqUri != expectedRabbitMqUri {
+		t.Errorf("Expected "+parameterName+
+			" to be \"%s\" but got \"%s\" instead!",
+			expectedRabbitMqUri,
+			actualRabbitMqUri)
+	}
+}
+
+func TestRabbitMQURIDefaultValue(t *testing.T) {
+	validateStringParameter((&Config{}).RabbitMQURI(),
+		"amqp://guest:guest@localhost:5672/%2f",
+		"RabbitMQ URI",
+		t)
+}
+
+func TestRabbitMQURIFromEnvVar(t *testing.T) {
+	expectedRabbitMqUri := "amqp://user:password@example.com:5672"
+
+	os.Setenv("RABBITMQ_URI", expectedRabbitMqUri)
+
+	validateStringParameter((&Config{}).RabbitMQURI(),
+		expectedRabbitMqUri,
+		"RabbitMQ URI",
+		t)
+}
+
+func TestRabbitMQURIFromConfig(t *testing.T) {
+	expectedRabbitMqUri := "amqp://user:password@example.com:5672"
+
+	config := Config{config: &configPayload{RabbitMQURI: &expectedRabbitMqUri}}
+
+	validateStringParameter(config.RabbitMQURI(),
+		expectedRabbitMqUri,
+		"RabbitMQ URI",
+		t)
+}
+
+var expectedClient = &stdClient.Client{Name: "test_client",
+	Address:       "10.0.0.42",
+	Subscriptions: strings.Split("email,messenger", ",")}
+
+func validateClient(actualClient *stdClient.Client, t *testing.T) {
+	validateStringParameter(actualClient.Name,
+		expectedClient.Name,
+		"client name",
+		t)
+
+	validateStringParameter(actualClient.Address,
+		expectedClient.Address,
+		"client address",
+		t)
+
+	if !reflect.DeepEqual(actualClient.Subscriptions,
+		expectedClient.Subscriptions) {
+		t.Errorf("Expected client subscriptions to be \"%v\""+
+			" but got \"%v\" instead!",
+			expectedClient.Subscriptions,
+			actualClient.Subscriptions)
+	}
+}
+
+func expectedClientFromConfig(t *testing.T) {
+	config := Config{config: &configPayload{Client: expectedClient}}
+
+	validateClient(config.Client(), t)
+}
+
+func expectedClientFromEnvVars(t *testing.T) {
+	os.Setenv("SENSU_CLIENT_NAME", expectedClient.Name)
+	os.Setenv("SENSU_CLIENT_ADDRESS", expectedClient.Address)
+	os.Setenv("SENSU_CLIENT_SUBSCRIPTIONS",
+		strings.Join(expectedClient.Subscriptions, ","))
+
+	validateClient((&Config{}).Client(), t)
+}
+
+func TestChecksFromConfig(t *testing.T) {
+	expectedCheckCount := 2
+	config := Config{
+		config: &configPayload{
+			Checks: []*check.Check{&check.Check{}, &check.Check{}}}}
+
+	actualheckCount := len(config.Checks())
+
+	if expectedCheckCount != actualheckCount {
+		t.Errorf("Expected check count to be \"%d\" but got \"%s\" instead!",
+			expectedCheckCount,
+			actualheckCount)
+	}
+}
