c0f967810361633fca83beb2d5434cfcda32e78b
Alexis Montagne <alexis.montagne@gmail.com>
2016-12-06 16:34:23 -0500

    Update the github.com/upfluence/sensu-go dep

--- Godeps/_workspace/src/github.com/upfluence/sensu-go/sensu/check/check.go
+++ Godeps/_workspace/src/github.com/upfluence/sensu-go/sensu/check/check.go
@@ -8,14 +8,18 @@ const (
 )
 
 type Check struct {
-	Name        string    `json:"name,omitempty"`
-	Type        CheckType `json:"type,omitempty"`
-	Command     string    `json:"command,omitempty"`
-	Extension   string    `json:"extension,omitempty"`
-	Standalone  bool      `json:"standalone,omitempty"`
-	Subscribers []string  `json:"subscribers,omitempty"`
-	Handler     string    `json:"handler,omitempty"`
-	Handlers    []string  `json:"handlers,omitempty"`
-	Source      string    `json:"source,omitempty"`
-	Interval    int64     `json:"interval,omitempty"`
+	Name         string    `json:"name,omitempty"`
+	Type         CheckType `json:"type,omitempty"`
+	Command      string    `json:"command,omitempty"`
+	Extension    string    `json:"extension,omitempty"`
+	Standalone   bool      `json:"standalone,omitempty"`
+	Subscribers  []string  `json:"subscribers,omitempty"`
+	Handler      string    `json:"handler,omitempty"`
+	Handlers     []string  `json:"handlers,omitempty"`
+	Source       string    `json:"source,omitempty"`
+	Interval     int64     `json:"interval,omitempty"`
+	Occurrences  int64     `json:"occurrences,omitempty"`
+	Refresh      int64     `json:"refresh,omitempty"`
+	Dependencies []string  `json:"dependencies,omitempty"`
+	Notification string    `json:"notification,omitempty"`
 }
